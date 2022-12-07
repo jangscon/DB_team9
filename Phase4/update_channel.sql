3e43a101520a4e530a08998a5cad0b5f2d27ffa5
YoungWoo <70523625+jangscon@users.noreply.github.com>
2022-12-08 03:48:55 +0900

    channel 테이블에 thumbnaul link 추가

---
+++ Phase4/update_channel.sql
@@ -0,0 +1,269 @@
+ALTER TABLE channel ADD thumbnail VARCHAR(200);
+
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/ytc/AMLnZu86YVNCLGXq4VET5BzSCzVv6rYSfb_Q1dmVk5J6=s176-c-k-c0x00ffffff-no-rj'
+WHERE channel_name = '총몇명';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-1dqfeK4TMuFAeWHwi6zL9QSm_nyTwn5QPliFoWg=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '장삐쭈';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu86YVNCLGXq4VET5BzSCzVv6rYSfb_Q1dmVk5J6=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '써리의 영상툰';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/iXhWkuspzKwRRfCKdiXFvUAKIJAY-hhDMfWoNZ_Hz0D2ZHjqLdffXJLHxIfEKLQ45uEwibSNhw=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '짤툰';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu89L9o68VEmVpAL6y2nywcrngqa264G7BCp12CO0Q=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'kiu기우쌤';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/zYd6MW5OSDLSbUmt2PI1uBhOiGTzJW8f7bUbZOhg_M_45Y8h0Kze9_Grx-XGF67MesRLXJkBcA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '헤어몬 Hairmon';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/KmZm7Ivpac7ZBE4asyJFtLTcWSwWi9zzV22mXv_sBFZ8syLvSrmDNOfNc9CbHXkBZx5vjAIVNA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '금강연화';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/Nf6Ja8R0UGEbQehYG03Us4qVN-APnbbx-1sxs-xzVFLPIYJSAx0GnkTPBG0yV4YBMpTtm2lfJw=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '차홍 CHAHONG';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-3QXdAWW04fUPAMi3gg0-LWCdHlOq_bMRikrUqmA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'LeoJ Makeup';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu99r7eLFliXpoVwRFkeLFSox5HiHOYwxYjHA8oSBA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'RISABAE';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu9ZiEb39nQGwgasvr_vU1WlJkstKbVTZrfZ_huWIQ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '조효진 HYOJIN';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu_2dht3hXIGafzOm33-XdU6uEI1XJR1jgM_ELSZ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '회사원A';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/CC3lKLV6DIJabtrU96HjtyXK4f8ajDUwTvlApdYS8QA_HniNBX9h0ICqNEDbOZs6La7FRwWE=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '숏박스';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/ytc/AMLnZu8L0rT3r5fQDAA-mbty1XFYzNzRiReuG8cL9T7W=s176-c-k-c0x00ffffff-no-rj'
+WHERE channel_name = '피식대학Psick Univ';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/ytc/AMLnZu8Ia8DsIhh46F6WWu1xhktgEfbSZgSo8y-02K9dmQ=s176-c-k-c0x00ffffff-no-rj'
+WHERE channel_name = '빠더너스 BDNS';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/ytc/AMLnZu9EQbCqMc64pHJksg6u85hvroGDPfAXh80veR0=s176-c-k-c0x00ffffff-no-rj'
+WHERE channel_name = '스튜디오 와플 - STUDIO WAFFLE';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/ytc/AMLnZu-vNYIUBoQv2lTdqOybPBa_7YvYH-aAsDodmcwpyA=s176-c-k-c0x00ffffff-no-rj'
+WHERE channel_name = 'MTN 머니투데이방송';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu_KptPhlZ024JCVEU_eptEnciU89Hp_MNcGL2Vl=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '슈카월드';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu9JDGkpG5asnPhVbhcbkbmPwBxFIWuzgZYsAymNGQ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '세바시 강연 Sebasi Talk';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/JwJ-Sbw0KP-qECIq7-Tx9hmMB1aezWMS7tZAfIJXwxbrAm5dlNn-MdDsE2jDXtEHAmLPUqOT4w=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '한국경제TV';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu8hdQ4TWLl6M_wFDBQmQpSiK_GEhTgazzqGnL8u=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '김시선';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-3tM4iM_CSnPQhRwLHfVbhyB0LERADf_klqo9efg=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '지무비 : G Movie';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/_jWWV0MMAXeyUV1OqC8SZ4c6cqin7kayz5Pn2IM6j0Dvm2lu9MfOq4UIQ9eWBCVa2TWs3jsYKOI=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '달빛뮤즈';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu_oE_kg1wJ5TF32_gvJbS2yqGmpO4n1FkuwRwsDaw=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '드림텔러(DreamTeller)';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-GwindKGGI79Q-TbHyvyu5204IBPdPDXODu0M4Lg=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '사나고 Sanago';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/rFje3ib8HFm-qoif8U5p9UVW0x5mPjWnrOl8JsmP2fhuCUaoOwu7YsbmJZXWqR25oYIIU0SEpA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '딱지 DDAKG';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/Xft4D1pHG7LvAcK0wOwa7If8ydBPUrKuJcwAS-nlDd1H2wzcsvdthS3mYFzbEsfUMid9O8RDbA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '긱블 Geekble';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-NWuoI1uspY1WRXq4nauWV7hfr3S95jfLFeJ4W=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '공돌이 용달';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu9TFykrGiTUJ_3x08GU7IioOfbaBXjMMDvoN4G2=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '오은영의 버킷리스트';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/XOVtVhgfiI7uZDRmjB2ae-EYK_RJX4fF0DsT9mXomzOVuEj0_bRD-C1RK4H7gaWX25Vl03rwMA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'EBSDocumentary (EBS 다큐)';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/MMhJ2rDZ6aR22IesH4Of24ZCTa_uFS7R_MqBYqVG8aT7QuZ22nvLel4c3f4tOfaTHcagnd_TYg=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '알면 자랑할 수 있는 지식';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu8ZloN_CNYd6mCAwu-Exo30Zgozew31dHgRg27x=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '지식 브런치';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu_VpSrtE2pv8hRf3CJXGwz0pbBIG2EsyWYxzHcCAQ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '스타일가이드 최겨울';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/ckUAx9WbZU8uDwzZrUwzC_5mm7fCbF2x04oUcU5t_AGGyBEI2-K5-UwzDLkt4zEu53I-Q2H_FA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '디렉터 짱구대디';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-rpa74qCPX02D4OF1NHb9Qbwb19MZlpV9LYQ99Qw=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '오늘의 주우재';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu8i1Pn_A2g9ru1wf1RaOE-jduiCJL2lX3WAabrb=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '깡스타일리스트';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu_x4cDqBlu11HMji0kbSwwVYs2t-uCnhSFSHcCX9g=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '[햄지]Hamzy';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/_s3C7XpwVKVr_5gDrmYJh9AOkU3wFlY9FWyZBVGVP_v7B09P5O4bbEZaWGpiuyT78Dk-aElE=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'tzuyang쯔양';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/sL5ugPfl9vvwRwhf6l5APY__BZBw8qWiwgHs-uVsMPFoD5-a4opTJIcRSyrY8aY5LEESOMWJ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '히밥heebab';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu8ZFN8wO_CCi5MSvDjoCxMqJ2fVEJllshSdBYXd=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '떵개떵';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/P81q6VK1IUQ11dOD06hoKs66GSMVoCJUhFB3z3GH414HIiKERR9U4yy_lcJVZO3uOFRJz35i=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '한세 HANSE';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu85bZ0NnFtESMrW8HXZObt0xbiI6GDTDEAZN-fyoQ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '[윤이련]50년 요리비결';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu90_2JrU8atSX93O2QYfLxTRlgrarriUxbXBWXvId8=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'Mrs macarons마카롱여사';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/lLeyS2vhyBSpkk7-JwNgPkS96Z7a7lJ9EagA2W9BJW-yJJXCE0k8oPyYOxvUnAAvl4LFhZ1mZuU=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '1분요리 뚝딱이형';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/MzQiCtGknvdDlOqokMKtjuOcfiX_BN347AawsHOOsa9c6g9WJWNVlPTXdfn3R2jV9rW_mbCdrHQ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'Official Dopa';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu88fElvXTdbMhtfvYjqbMGb3ULvOF_jPcBM_LPhIA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '랄로';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu8vfnIwP9wU7aQfJcivfcPjNRkowX9LbvhcrVvN6g=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'PAKA';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu84oZmNhLWpU1qaLbKTLXRO4VSNqVh4DOkSEGfJDQ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '괴물쥐 유튜브';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu_bJLM9GGcjTEsGiDSRewy2b-vqz_HqaHWJlSuvCg=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '김소형 채널H';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/9A-FdHhJtrtPHSi0A1ZsyzeHnbxTzeqz25fDF9TJwtFw8YntPeyciOA5UH-erSxG3Vs5j-OcdOc=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '의학채널 비온뒤';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/DcE-B_n4E1UrbA7aeoAkgMOG_fTRp7ogJgeNopsmIu6Pfu5g9o359R2hTHs96Hb6x_rTZhRu=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '닥터프렌즈';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/LU7LcBMHFTm_5g65_DF2bvwUbw0CZa0ao9MwbGY18kfJX2c5SS-9Qb-OnwPzJvxIDGvuyRqE0U0=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '닥터조물주 꽈추형';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-0tFgTkMCJhDBZVD8cU7HeG7bC6eprh1mEnvr2WA0=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '말왕TV';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/LzBr3K1XPUs1fEwJA1dpcIJNQo7q0gsR3EIwCFFbrH8pZi5AUNYH2iGEE0R5AsMUlR_1J3QnWGE=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '김종국 GYM JONG KOOK';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu83-5or1HaIln7R1dxZ3te2xGAoRwhS6cAdsDzCtw=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '피지컬갤러리';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/4mnz1c9zO8i1ZrPaNSiwLZkersApxZpQIvBiYnxcHGQQmIhn-uFBn0tBnK2kuoX0jakfzejD0CM=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '지기TV';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-aHTHvY_WU4Cj6URDgF6ufqmlDHQHTFtgNJTqADA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'MBCkpop';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/6xOBiHC0rSAa74kQ8MLBDW_sYN0KRCebJGlGODREsjypB9zOEx63TXR1oSslLLe9ptDIAcjV6Q=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'KBS Kpop';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/Uxpz5J0EcsFJRbqh4Ip7i3TTNsxTh5jVUxfZmV1DTrCQM_ihfzBGMmkfSRGWoFK9M0anhIie=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '스브스케이팝 X INKIGAYO';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/NLKnIGLdCfPuDOm-VDJWIPsX5Mr8bG9ea9IZ1hgAjHvLF5wR720CGGaUFHRbzQ5Towdb_-iHEio=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'Mnet TV';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/gsNDSfvzKpZT0OEjTBFH5PxokZ7WeZQU5PcsMWD8vJyBI6ts4uUDic_KsipnVbIUhtToblLaig=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'KBS News';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/w7ColCSgsKVMJuh6izq4oRlYn-ZkJ8roQuHQy1Nvc75ONCH7uzLmoUE5YMJtZqFCHb4ZFPxAOg=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'SBS 뉴스';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-1RKHNUjQPUH705bU4LYTXwIYlnMxyFWngAhuEkw=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'JTBC News';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/ezG7fZAoI4puqxZvEHRY0Xfwl6CcR3bWRadwh-lg_-JSuBw9aK6hpJuR5wIlu1WHhsvseAWzfyA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'YTN';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/S69D4Y1cEP1tJwx1Kys1BUUmAVIqsPB9TKRR97_kv-4xcmce7zldi_9Bu-k3VSKSd5bF6UzlmA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '브릿센트 x 영국영어';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/UKlav7ghQYdNmLwjkU3rwmzkwrdYVoqIIMM3aVpgPKoogBvkqJtK-nNniWuXYIpf-pG9QLiiNA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '코빨간배춘기 [배성재의 텐]';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu9wFhWJGg9jxgMTJdNcymN5SFc_ceTwF-EDSic1Cg=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'SBS Radio 에라오';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu8XuxuJnD9cbDlRB65ghg5edgiWPha-6Yc7mq_YZw=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'YTN 라디오';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-W3ZFj4y29iDZwNBedkIaiFYNLBu1UkwJx_fWXrg=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '스포츠타임';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-2GPZIXZ4JSQ4Rl9fxMQUEdIgDRLFwSDYdqrJTbQ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '프로동네야구 PDB';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/1Vep-oBNSDN06JiDlYPMc7WNmMbVK8bC6EcWUkNrh54m35rgnCzKlbD5cMUNF7Uh3O4_yLNjWw=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '슛포러브';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-768w81o2_um1einmXC73_J69S8AD4qM9DM-dbuQ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'SPOTV';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-wqNEZkaT1naa0bFTW8UJb0Rg35H5NkdjRN94ccA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'ITSub잇섭';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu9tQqhh5Ssp6mKmjVzYaFY2SLJKDEG0h3JP60fdyMs=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '테크몽 Techmong';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-74w6tc0fbMjfA4T-jWDBRTxP7beQGjSNlIU1Trg=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'UNDERkg';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-iERWyplaHHvZqqjsQh82u6aMndIu6iJ-nnv63dg=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '눈쟁이';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-UWvwHCKwBxSfbMOjy5D2z03jrZz4hnOWyksk1gw=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '이지금 [IU Official]';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-pp4aGx651l3ntwq1Kb4-fHqNVrHTPAOJcxlcBIA=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '토모토모TomoTomo';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/kQ1UQswx536TZOymhmuRjO0bobMI4AQMlo51NWn1B9M2kGmBJCIB7wwt4HapEPNUZ0EO1Bzvng=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '가요이 키우기 YoiKi';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/k0VEDu0uau2Mxl3DyxkrU5a9K2EPJsrKshXaiCpCvBCMDq8S6fnkwaPCZD9jekuuKgZPZcCoTw=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '엔조이커플enjoycouple';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/0az-_7XNg5Ezsye0xz9ULwR8Bj3XcPPEIeyyxITK0UOjTryJOEPTpklfzgdxGyEvHoFXBqMT-A=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '사물궁이 잡학지식';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-FPJv66wbPdjQASHYZEprUYQOGgulX_HvVyj5c=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '안될과학 Unrealscience';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu8FA2igSvFnefdP3NBCiLCOEHAlh1UVLUgHbYTJ6Q=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '리뷰엉이: Owl''s Review';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu_pccKCtjCbZIAEahG-ghmqsRr0ExBGgOwfoIX1lQ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '과학드림 [Science Dream]';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu-96Up11iRP0gPUQXCBpbyQTtvBaJQRkQM8Ls6cmQ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '1MILLION Dance Studio';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/iga__5ycaNtPIpDNRbK9JjCDGLKqJhCqZmGL4Ct8-nUH4PPh9PIwm3PVZrakaepZNfffVGq6BQ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'VIVA DANCE STUDIO';
+UPDATE channel
+SET thumbnail = 'https://yt3.ggpht.com/91I9zVP3bi_Uig9XCzfBQOlyOuJCoo8k4qxKscSbX82ugr9kX-HXC533nLZWYy-Ual-zgvfHvg=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = '땡깡DanceKang';
+UPDATE channel
+SET thumbnail = 'https://yt3.googleusercontent.com/ytc/AMLnZu878_uwUtMp27dVenHrsmweT9UQaHI74Qmt7ZBMFQ=s176-c-k-c0x00ffffff-no-rj-mo'
+WHERE channel_name = 'Aiki아이키릿';
+
+
+commit;
